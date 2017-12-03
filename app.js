27bad0cd04e2bbf9bccd1b77f5ee07545fb99148
MVytautas <vmikalainis@gmail.com>
2017-12-03 19:03:15 +0200

    working on index
    routing changes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var fs = require('fs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +15,16 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+//app.set('view engine', 'ejs'); // template engine
+//app.engine('html', require('ejs').renderFile); // turn engine to use html
+
+app.get('/', function(request, response) {
+  var readFile = "client/index.html";
+  var fileContents = fs.readFileSync(readFile);
+  
+  response.send(fileContents.toString());
+  });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
